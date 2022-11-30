7c98a0138bac00f8f260ea136759b5e6f6fd0fcf
SharonMosesJangam <sharonmoses96@gmail.com>
2022-11-30 15:48:45 -0700

    plugging logger changes to passthru

--- cmd/passthru/main.go
+++ cmd/passthru/main.go
@@ -3,19 +3,17 @@ package main
 import (
 	"context"
 	"flag"
-	"fmt"
+	//"fmt"
 	"os"
 	"os/signal"
 	"sync"
 	"time"
 
-	"go.uber.org/zap",
-	"go.uber.org/zap/zapcore",
 	"github.com/gaukas/passthru/config"
 	"github.com/gaukas/passthru/handler"
 	"github.com/gaukas/passthru/protocol"
 	"github.com/gaukas/passthru/protocol/tls"
-	"github.com/gaukas/internal/logger"
+        "github.com/gaukas/passthru/internal/logger"
 )
 
 var (
@@ -33,13 +31,25 @@ var (
 // STOP EDITING! OR YOU ARE HACKING THE PROJECT.
 
 func main() {
-
-	filename := "logs.log"
-	log := logger.fileLogger(filename)
+        logger.InitLogger("test.log", true, logger.LOG_DEBUG)
 	configFile := flag.String("c", "", "path to config file")
 	workerCountPerServer := flag.Int("w", 10, "number of workers (default 10, 0 for unlimited) assigned for each server")
 	workerTimeout := flag.Duration("t", 5*time.Second, "worker timeout in seconds (default 5)")
 	flag.Parse()
+
+	// Disable worker-based concurrency for now
+	if *workerCountPerServer != 0 {
+		//fmt.Println("Worker-based concurrency is not enabled for this build. Automatically set to 0.")
+                logger.Debugf("Worker-based concurrency is not enabled for this build. Automatically set to 0.")
+	}
+	*workerCountPerServer = 0
+
+	// Must set config file
+	if *configFile == "" {
+		//fmt.Println("Config file is not set. Use -c to set config file.")
+                logger.Errorf("Config file is not set. Use -c to set config file.")
+		os.Exit(1)
+	}
 
 	// Load config
 	var err error
@@ -51,14 +61,14 @@ func main() {
 	// Check version
 	switch conf.Version.CanFitInServer(serverVersion) {
 	case config.WONT_FIT:
-		//panic("[FATAL] config version is too new for the server.")
-		log.Fatal("config version is too new for the server.")
+		panic("[FATAL] config version is too new for the server.")
+                logger.Fatalf("config version is too new for the server.")
 	case config.MAY_FIT:
 		//fmt.Println("[WARNING] config version is newer than the server. Some features may not work.")
-		log.Warn("config version is newer than the server. Some features may not work.")
+                logger.Warnf("config version is newer than the server. Some features may not work.")
 	case config.SHOULD_FIT:
 		//fmt.Println("[INFO] config version is better patched than the server. There could be unintended bahaviors.")
-		log.Info("config version is better patched than the server. There could be unintended bahaviors.")
+                logger.Infof("config version is better patched than the server. There could be unintended behaviors.")
 	}
 
 	bufServer := make(chan *handler.Server, len(conf.Servers))
@@ -100,13 +110,15 @@ func main() {
 						err := server.HandleNextConn(ctxTimeOut)
 						switch err {
 						case handler.ErrUnknownAction:
-							fmt.Println("[WARNING] unknown action from a protocol.Protocol")
+							//fmt.Println("[WARNING] unknown action from a protocol.Protocol")
+                                                        logger.Warnf("unknown action from a protocol.Protocol")
 							continue
 						case handler.ErrServerStopped:
 							return
 						default:
 							if err != nil && err != context.DeadlineExceeded {
-								fmt.Printf("[ERROR] error while handling connection: %v\n", err)
+								//fmt.Printf("[ERROR] error while handling connection: %v\n", err)
+                                                                logger.Errorf("error while handling connection: %v", err)
 							}
 						}
 					}
@@ -115,8 +127,8 @@ func main() {
 		}
 
 		//fmt.Printf("[INFO] server %s started\n", serverAddr)
-		log.Info("server started ", zap.String("Server Addr:", serverAddr))
-		bufServer <- server
+		logger.Infof("server %s started\n", serverAddr)
+                bufServer <- server
 	}
 	close(bufServer)
 
